9e9ecca1f0c6196e51891cce8cc224ca4021a733
Hide1204 <hirahide1204@gmail.com>
2021-06-07 11:57:16 +0900

    いらないファイルを削除

--- test/src/test/java/com/example/PlayerCharacterTest.java
+++
@@ -1,9 +0,0 @@
-public class PlayerCharacterTest {
-
-    @Test
-    public void 右へ移動() {
-        PlayerCharacter PC= new PlayerCharacter(10,10);
-        PC.moveRight();
-        assertEquals(11, PC.get_x());
-    }
-}
